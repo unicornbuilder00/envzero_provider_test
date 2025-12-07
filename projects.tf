51f2773db4f008b53941ad732eb58a42327ec452
unicornbuilder00 <163330536+unicornbuilder00@users.noreply.github.com>
2025-12-07 16:47:22 -0500

    Rename projects in projects.tf for clarity

--- projects.tf
+++ projects.tf
@@ -1,9 +1,9 @@
-data "env0_project" "default_project" {
+tdata "env0_project" "default_project" {
   name = "JC Bootstrap Test"
 }
 
 resource "env0_project" "Projects_Bootstrap" {
-  name              = "Templates BootStrap"
+  name              = "Projects BootStrap"
   description       = "Project for Bootstrapping Projects"
   parent_project_id = data.env0_project.default_project.id
 }
@@ -21,7 +21,7 @@ resource "env0_project" "Environments_Bootstrap" {
 }
 
 resource "env0_project" "Variables_Bootstrap" {
-  name              = "Environments Bootstrap"
+  name              = "Variables Bootstrap"
   description       = "Project for Bootstraping Variables"
   parent_project_id = data.env0_project.default_project.id
 }
