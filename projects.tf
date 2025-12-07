1557b2cd789a0c217db51662229f12dff7fa42a8
unicornbuilder00 <163330536+unicornbuilder00@users.noreply.github.com>
2025-12-07 16:44:20 -0500

    Add bootstrap projects and environments resources

--- projects.tf
+++ projects.tf
@@ -2,14 +2,26 @@ data "env0_project" "default_project" {
   name = "JC Bootstrap Test"
 }
 
-resource "env0_project" "new_team_project" {
-  name              = "JC Test Project 1"
-  description       = "Project for team infrastructure"
+resource "env0_project" "Projects_Bootstrap" {
+  name              = "Templates BootStrap"
+  description       = "Project for Bootstrapping Projects"
   parent_project_id = data.env0_project.default_project.id
 }
 
-resource "env0_project" "new_team_project_2" {
-  name              = "JC Test Project 2"
-  description       = "Project for team infrastructure #2"
+resource "env0_project" "Templates_Bootstrap" {
+  name              = "Templates Bootstrap"
+  description       = "Project for Bootstrapping Templates"
+  parent_project_id = data.env0_project.default_project.id
+}
+
+resource "env0_project" "Environments_Bootstrap" {
+  name              = "Environments Bootstrap"
+  description       = "Project for Boostrapping Environments"
+  parent_project_id = data.env0_project.default_project.id
+}
+
+resource "env0_project" "Variables_Bootstrap" {
+  name              = "Environments Bootstrap"
+  description       = "Project for Bootstraping Variables"
   parent_project_id = data.env0_project.default_project.id
 }
