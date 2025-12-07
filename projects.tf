c0b0cc0fd7d79fb31b5511ffc36ce8786286fc88
unicornbuilder00 <163330536+unicornbuilder00@users.noreply.github.com>
2025-12-07 16:36:14 -0500

    Add new team project resource for JC Test Project 2
    Added a second project resource with a unique name and description.

--- projects.tf
+++ projects.tf
@@ -3,7 +3,13 @@ data "env0_project" "default_project" {
 }
 
 resource "env0_project" "new_team_project" {
-  name              = "JC Test Projects Project"
+  name              = "JC Test Project 1"
   description       = "Project for team infrastructure"
+  parent_project_id = data.env0_project.default_project.id
+}
+
+resource "env0_project" "new_team_project" {
+  name              = "JC Test Project 2"
+  description       = "Project for team infrastructure #2"
   parent_project_id = data.env0_project.default_project.id
 }
