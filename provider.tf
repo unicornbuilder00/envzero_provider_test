a533b4e4c6e8071db62a08681a41ef2a01e3a341
unicornbuilder00 <163330536+unicornbuilder00@users.noreply.github.com>
2025-12-07 13:27:28 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,3 +8,18 @@ terraform {
 }
 
 provider "env0" {
+  api_key    = var.env0_api_key
+  api_secret = var.env0_api_secret
+}
+
+variable "env0_api_key" {
+  type        = string
+  description = "env0 API Key"
+  sensitive   = true
+}
+
+variable "env0_api_secret" {
+  type        = string
+  description = "env0 API Secret"
+  sensitive   = true
+}
