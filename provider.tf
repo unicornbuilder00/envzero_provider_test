2ed6024d5fa32f9266ef83467d38a1c5ea7c9c19
unicornbuilder00 <163330536+unicornbuilder00@users.noreply.github.com>
2025-12-07 13:29:02 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,18 +8,5 @@ terraform {
 }
 
 provider "env0" {
-  api_key    = var.env0_api_key
-  api_secret = var.env0_api_secret
-}
-
-variable "env0_api_key" {
-  type        = string
-  description = "env0 API Key"
-  sensitive   = true
-}
-
-variable "env0_api_secret" {
-  type        = string
-  description = "env0 API Secret"
-  sensitive   = true
+  # Credentials will be read from ENV0_API_KEY and ENV0_API_SECRET environment variables
 }
