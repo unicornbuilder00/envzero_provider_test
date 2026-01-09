28e50fc5a31debb724d4678272467b79af3efb8a
unicornbuilder00 <163330536+unicornbuilder00@users.noreply.github.com>
2026-01-09 13:10:32 -0500

    Fix repository URL format in templates.tf

--- templates.tf
+++ templates.tf
@@ -2,7 +2,7 @@ resource "env0_template" "my_template" {
   name        = "JC-TEMPLATE-TEST"
   description = "I PRAY THIS WORKS"
   type        = "terraform"
-  repository  = :https://github.com/unicornbuilder00/envzero_provider_test"
+  repository  = "https://github.com/unicornbuilder00/envzero_provider_test"
   revision    = "main"
   
   terraform_version = "1.5.0"
