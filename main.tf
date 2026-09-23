7c98a1aa71196b9b648f9beba763ac78ef73e176
unicornbuilder00 <163330536+unicornbuilder00@users.noreply.github.com>
2026-09-23 16:44:09 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    env0 = {
+      source = "env0/env0"
+    }
+  }
+}
+
+# Auth comes from ENV0_API_KEY and ENV0_API_SECRET environment variables
+provider "env0" {}
+
+variable "project_name" {
+  type        = string
+  description = "Name of the env0 project to create"
+  default     = "my-new-project"
+}
+
+variable "project_description" {
+  type    = string
+  default = "Created by env0 using Terraform"
+}
+
+resource "env0_project" "this" {
+  name        = var.project_name
+  description = var.project_description
+}
+
+output "project_id" {
+  value = env0_project.this.id
+}
